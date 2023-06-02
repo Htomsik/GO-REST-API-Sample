64ef4ebdbcdc901db4e544a17dc1e2c626b8a4d2
Htomsik <JessJake@proton.me>
2023-06-03 01:38:48 +0300

    Fetch: Move to SQLite3

--- internal/app/apiServer/apiServer.go
+++ internal/app/apiServer/apiServer.go
@@ -3,9 +3,15 @@ package apiServer
 import (
 	"database/sql"
 	"github.com/Htomsik/GO-REST-API-Sample/internal/app/store/sqlStore"
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/sqlite3"
+	_ "github.com/golang-migrate/migrate/source/file"
 	"github.com/gorilla/sessions"
+	_ "github.com/mattn/go-sqlite3"
 	"net/http"
 )
+
+const migrationsPath = "file://migrations"
 
 // Start ...
 func Start(config *Config) error {
@@ -31,6 +37,26 @@ func newDB(databaseType string, databaseUrl string) (*sql.DB, error) {
 	db, err := sql.Open(databaseType, databaseUrl)
 
 	if err != nil {
+		return nil, err
+	}
+
+	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	migration, err := migrate.NewWithDatabaseInstance(
+		migrationsPath,
+		databaseType,
+		driver,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := migration.Up(); err != nil && err != migrate.ErrNoChange {
 		return nil, err
 	}
 
